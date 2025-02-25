a708cee04c5ad7883850e06d509bfd0916021450
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-02-25 16:07:37 +0600

    Listing is on the process, it will done after when i will setup the database

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -3,6 +3,11 @@ import React from "react";
 import carDetails from "./../Shared/carDetails.json";
 import InputField from "./components/InputField";
 import DropdownField from "./components/DropdownField";
+import { Textarea } from "@/components/ui/textarea";
+import { Separator } from "@/components/ui/separator";
+import features from "./../Shared/features.json";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 function AddListing() {
   return (
     <div>
@@ -22,15 +27,34 @@ function AddListing() {
                   </lable>
                   {item.fieldType == "text" || item.fieldType == "number" ? (
                     <InputField item={item} />
-                  ):item.fieldType=="dropdown"?<DropdownField item={item}/> : null}
+                  ) : item.fieldType == "dropdown" ? (
+                    <DropdownField item={item} />
+                  ) : item.fieldType == "textarea" ? (
+                    <Textarea item={item} />
+                  ) : null}
+                </div>
+              ))}
+            </div>
+          </div>
+          <Separator className=" my-6 " />
+          {/* feature list  */}
+
+          <div>
+            <h2 className="font-medium text-xl my-6">Features</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+              {features.features.map((item, index) => (
+                <div key={index} className="flex gap-2 items-center">
+                  <Checkbox></Checkbox>
+                  <h2>{item.label}</h2>
                 </div>
               ))}
             </div>
           </div>
 
-          {/* feature list  */}
-
           {/* car details */}
+          <div className="mt-10 flex justify-end">
+            <Button>Submit</Button>
+          </div>
         </form>
       </div>
     </div>
