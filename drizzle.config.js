b669bb517184b26952e2582c89d3f8669862f6bb
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-07-07 15:48:54 +0600

    s

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,22 +1,9 @@
-// /** @type { import("drizzle-kit").Config } */
-// export default {
-//   schema: "./configs/schema.js",
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: "postgresql://neondb_owner:npg_s5AxjU3DIaiR@ep-icy-paper-a8v2b2z1-pooler.eastus2.azure.neon.tech/autoBazar?sslmode=require&channel_binding=require",
-//   },
-// };
-
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
+/** @type { import("drizzle-kit").Config } */
+export default {
   schema: "./configs/schema.js",
-  out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.VITE_DATABASE_URL,
+    url: "postgresql://neondb_owner:npg_s5AxjU3DIaiR@ep-icy-paper-a8v2b2z1-pooler.eastus2.azure.neon.tech/autoBazar?sslmode=require&channel_binding=require",
   },
-  verbose: true,
-  strict: true,
-  port: 4984, // Use a different port
-});
+};
+
