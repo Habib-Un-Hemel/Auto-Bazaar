e605cacdd291564adc29b0f87900321af243f521
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-07-14 02:52:04 +0600

    update the catergory

--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -45,7 +45,8 @@ function MostSearchedCar() {
   }
 
   return (
-    <div className="mx-24 mt-60">
+    // <div className="mx-24 mt-60">
+    <div className="mx-24 ">
       <h2 className="font-bold text-3xl text-center mt-16 mb-7">
         Most Searched Cars
       </h2>
